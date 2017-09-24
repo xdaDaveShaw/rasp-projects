51709352ae876e91282eb7953d8f8c0a9fc0a897
Dave Shaw <dave@taeguk.co.uk>
2017-09-24 17:37:57 +0100

    Make all LEDs pulse

--- stat.py
+++ stat.py
@@ -7,10 +7,8 @@ sb.on()  # all leds on
 sleep(1)
 sb.off()  # all leds off
 sleep(1)
-sb.one.lights.green.value = 0.5  # green led of first strip at half brightness
-sleep(1)
-#sb.two.value = (0.5, 0.5)  # both leds of second strip at half brightness
-sleep(1)
-sb.one.lights.pulse()  # both leds of first strip fading in and out
-sleep(1)
-sb.two.lights.pulse()  # both leds of second strip pulsing in opposite timing with the first
+for strip in sb:
+    strip.lights.red.pulse()
+    sleep(1)
+    strip.lights.green.pulse()
+    sleep(1)
