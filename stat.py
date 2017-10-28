925c4f0dc12212ea9ae0993b80bdd60ab2e688c8
Dave Shaw <dave@taeguk.co.uk>
2017-10-28 10:54:22 +0100

    Playing with the buttons on the status board

--- stat.py
+++ stat.py
@@ -3,12 +3,24 @@ from time import sleep
 
 sb = StatusBoard(pwm=True)
 
-sb.on()  # all leds on
-sleep(1)
-sb.off()  # all leds off
-sleep(1)
-for strip in sb:
-    strip.lights.red.pulse()
+#DAS: Doesn't quite stop right yet
+keepGoing = 1
+def stop():
+    keepGoing = 0
+    
+sb.five.button.when_pressed = stop
+
+while keepGoing == 1:
+    sb.on()  # all leds on
     sleep(1)
-    strip.lights.green.pulse()
+    sb.off()  # all leds off
     sleep(1)
+    sb.one.button.wait_for_press()
+    for strip in sb:
+        strip.lights.red.pulse()
+        sleep(1)
+        strip.lights.green.pulse()
+        sleep(1)
+    sb.four.button.wait_for_press()
+    sb.off()
+    
