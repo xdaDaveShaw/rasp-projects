95ce09d8035d8296a53fe493cacbfe102c7e13f5
Dave Shaw <dave@taeguk.co.uk>
2017-12-17 16:12:05 +0000

    complete client (except rand)

--- tree-berry/client.py
+++ tree-berry/client.py
@@ -1,14 +1,32 @@
 import socket
+from gpiozero import StatusBoard
+from signal import pause
 
-TCP_IP = '192.168.0.29'
-TCP_PORT = 5005
-BUFFER_SIZE=1024
-MESSAGE= "Start"
+def send(name):
+	TCP_IP = '192.168.0.29'
+	TCP_PORT = 5005
+	BUFFER_SIZE=1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(name)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
+def one_p():
+	send("up.py")
+def two_p():
+	send("blink.py")
+def three_p():
+	send("flicker.py")
+def four_p():
+	send("seq.py")
 
-print "rcv'd", data
+sb = StatusBoard()
+
+sb.one.button.when_pressed = one_p
+sb.two.button.when_pressed = two_p
+sb.three.button.when_pressed = three_p
+sb.four.button.when_pressed = four_p
+
+pause()
+
