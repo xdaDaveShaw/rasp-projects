6f8c4320f7731edd41e7e80c67fe91c48ca21ded
Dave Shaw <dave@taeguk.co.uk>
2017-12-17 14:46:50 +0000

    rewritten server

--- tree-berry/server.py
+++ tree-berry/server.py
@@ -1,20 +1,15 @@
-import socket
+import SocketServer
 
-TCP_IP = '192.168.0.29'
-TCP_PORT = 5005
-BUFFER_SIZE = 20
+class TcpHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+if __name__ == "__main__":
+    TCP_IP = '192.168.0.29'
+    TCP_PORT = 5005
 
-conn, addr = s.accept()
+    server = SocketServer.TCPServer((TCP_IP, TCP_PORT), TcpHandler)
 
-print "connection address:", addr
-
-while True:
-	data = conn.recv(BUFFER_SIZE)
-	if not data: break
-	print "received data:", data
-	conn.send(data) #echo
-conn.close()
+    server.serve_forever()
