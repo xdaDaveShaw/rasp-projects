047068619f3913b9420d3a3c99670ddd9c235bbc
Dave Shaw <dave@taeguk.co.uk>
2017-12-17 14:35:02 +0000

    fixed typoes

--- tree-berry/server.py
+++ tree-berry/server.py
@@ -4,13 +4,13 @@ TCP_IP = '192.168.0.29'
 TCP_PORT = 5005
 BUFFER_SIZE = 20
 
-s = socket.socket(socket(AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
 
-print 'connection address:", addr
+print "connection address:", addr
 
 while True:
 	data = conn.recv(BUFFER_SIZE)
