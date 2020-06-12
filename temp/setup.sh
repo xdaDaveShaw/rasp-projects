df8ab90d0464bbb4de7228f7110520f4b9b452be
Dave Shaw <dave@taeguk.co.uk>
2020-06-12 11:40:02 +0100

    Improve BME setup

--- temp/setup.sh
+++ temp/setup.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 sudo pip3 install --upgrade setuptools
 sudo pip3 install adafruit-io
 sudo apt install rng-tools -y
-curl https://get.pimoroni.com/bme680 | bash
+curl https://get.pimoroni.com/bme680 > bme680
+more bme680
+bash bme680
+rm bme680
 
